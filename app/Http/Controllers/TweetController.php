f9522c2acedd0940d5fa33749dc9754fb6ba33dd
erikbelusic <erikbelusic@outlook.com>
2016-11-04 17:51:05 -0400

    Update skill response

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Tweet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -9,6 +10,43 @@ class TweetController extends Controller
 {
     public function store(Request $request)
     {
-        Log::info($request->all());
+        if(env('ECHO_APP_ID') !==  $this->getApplicationID($request)) {
+            return abort(403);
+        }
+
+        $tweet = Tweet::create(['content' => $this->getTweetContent($request)]);
+
+        $response = [
+            'version' => 1.0,
+            'response' => [
+                'outputSpeech' => [
+                    'type' => 'PlainText',
+                    'text' => "Your message was tweeted: $tweet->content"
+                ],
+                'card' => [
+                    'type' => 'Simple',
+                    'title' => '@AlexCummunity on twitter',
+                    'content' => "Your message was tweeted: $tweet->content"
+                ],
+                'shouldEndSession' => true
+            ]
+        ];
+
+        return response()->json($response);
+    }
+
+
+
+
+
+
+    protected function getApplicationID($request)
+    {
+        return $request->input('session')['application']['applicationId'];
+    }
+
+    protected function getTweetContent($request)
+    {
+        return $request->input('request')['intent']['slots']['Content']['value'];
     }
 }
